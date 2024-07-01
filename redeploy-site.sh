ae9fbcfceee6399c85893426e18650231d5276e2
satoshi-sh <satoss1108@gmail.com>
2024-07-01 17:47:03 -0500

    Fix typo

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,5 +2,5 @@ tmux kill-window -t flask-run
 cd mlh-portfolio
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
-pip install reuirements.txt
+pip install -r requirements.txt
 tmux new-session -d -s flask-run "flask run --host=0.0.0.0"
