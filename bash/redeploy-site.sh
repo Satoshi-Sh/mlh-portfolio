28d68f5612b85b1adabfc795470f6a02218dd3df
satoshi-sh <satoss1108@gmail.com>
2024-07-23 12:00:34 -0500

    Updated redeploy-site.sh to use docker

--- bash/redeploy-site.sh
+++ bash/redeploy-site.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+cd ./mlh-portfolio
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down # avoid memory issues on vps 
+docker compose -f docker-compose.prod.yml up -d --build
+
+
+
+# Before docker 
+# git fetch && git reset origin/main --hard
+# source python3-virtualenv/bin/activate
+# pip install -r requirements.txt
+# systemctl daemon-reload
+# systemctl restart myportfolio
